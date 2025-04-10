610219c4094fc573d55e593b94575ceb14aba2f6
Ellen Coutinho <ellencls@al.insper.edu.br>
2025-04-10 13:45:17 -0300

    Adiciona áudio

---
+++ Assets/InitialMusic.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class InitialMusic : MonoBehaviour
+{
+    private static InitialMusic instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class UIManager : MonoBehaviour
 {
     public GameObject endGamePanel;
+    private bool has_played = false;
+    AudioSource end_audio;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        end_audio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -15,6 +17,11 @@ public class UIManager : MonoBehaviour
     {
         if (GameController.gameOver)
         {
+            if (!has_played)
+            {
+                end_audio.Play();
+                has_played = true;
+            }
             endGamePanel.SetActive(true);
         }
     }
